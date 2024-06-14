784344f61d8627b5f1271d6dbc6f196e236744bd
LucasGatica <lucas.gatica79@gmail.com>
2024-06-14 12:12:41 -0300

    view controller
    adicionando protocols do view controller

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+    
 
+    @IBOutlet weak var viagensTableVIew: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viagensTableVIew.dataSource = self
     }
-
+    
+    
 
 }
+
+
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "Viagem \(indexPath.row)"
+        return cell
+    }
+    
+}		
 
